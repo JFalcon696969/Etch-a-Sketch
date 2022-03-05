c61f6c2bf5e0a3510e0886d2be5dacdfeec20e17
JFalcon696969 <jfalcon696969@gmail.com>
2022-03-05 13:49:32 +0000

    adding html, css and js

---
+++ script.js
@@ -0,0 +1,45 @@
+
+function createGrid(number){
+    let size = 500/number;
+
+    let sqNum = (size.toString()+"px ").repeat(number) 
+
+    let body = document.querySelector("body");
+
+    let container = document.createElement("div");
+    container.classList = "contain";
+    
+    container.style.display = "grid";
+    container.style.gridTemplateColumns = sqNum;
+    container.style.gridTemplateRows = sqNum;
+    container.style.border = "1px solid black";
+
+    for(let i=0; i < number**2; i++){
+        let div = document.createElement("div");
+        div.style.width = `${size}`;
+        div.style.height = `${size}`;
+        div.classList = "square";
+        div.addEventListener("mouseover", e => div.classList = "painted")
+        div.addEventListener("click", e => div.classList.remove("painted"))
+        container.appendChild(div);
+    }
+
+    body.appendChild(container);
+
+    return number
+}
+
+let oldNumber = createGrid(16);
+
+let clearBtn = document.querySelector(".clear");
+
+clearBtn.addEventListener("click", e => {
+
+    let number = prompt("Number of squares per grid:");
+    
+    let body = document.querySelector("body");
+    body.removeChild(document.querySelector(".contain"))
+
+    oldNumber = createGrid(number||oldNumber);
+    }
+);
